02b4154063de3355590157a19f9ea55233def252
Toma Bogdan <tbogdane@TBE>
2023-12-06 21:11:13 +0200

    data is now dislayed on principal weather card

--- src/modules/handleData.js
+++ src/modules/handleData.js
@@ -1,7 +1,76 @@
-const handleData = (current, forecast) => {
+import { temperatureUnits } from "./webpage";
 
+class WeatherDataHandler {
+  constructor(weatherData) {
+    this.todayWeather = new WeatherDay(weatherData.current);
+    this.forecastWeather = weatherData.forecast.forecastday.map(
+      (day) => new WeatherDay(day)
+    );
+  }
+
+  getDay0() {
+    return this.todayWeather;
+  }
+
+  getDay1() {
+    return this.forecastWeather[0];
+  }
+}
+
+class WeatherDay {
+  constructor(data) {
+    this.units = temperatureUnits;
+    this.todayTemp = data.temp_c;
+    this.feelsLike = data.feelslike_c;
+    this.wind = data.wind_kph;
+    this.windDir = data.wind_dir;
+    this.humidity = data.humidity;
+    this.pressure = data.pressure_mb;
+    this.uvLevele = data.uv;
+  }
+
+  changeUnits(data) {
+    if (this.units === "Celsius") {
+      this.units = "Fahrenheit";
+      this.todayTemp = data.temp_f;
+      this.feelsLike = data.feelsLike_f;
+      console.log("c to f");
+    } else {
+      this.units = "Celsius";
+      this.todayTemp = data.temp_c;
+      this.feelsLike = data.feelsLike_c;
+      console.log("f to c");
+    }
+  }
+}
+
+const updateData = (weatherData) => {
+  updateTodayCard(weatherData);
 };
 
-const updateData = () => {};
+const updateTodayCard = (weatherData) => {
+  const weather = weatherData.getDay0();
 
-export { handleData };
+  const todayTemp = document.getElementById("today-weather-temp");
+  todayTemp.textContent = `${weather.todayTemp} C`;
+
+  const tempFeel = document.getElementById("today-weather-feel");
+  tempFeel.textContent = `Feels like ${weather.feelsLike} C`;
+
+  const windSpeed = document.getElementById("today-weather-sct-wind-value");
+  windSpeed.textContent = weather.wind;
+
+  const windDir = document.getElementById("today-weather-sct-wind-dir-value");
+  windDir.textContent = weather.windDir;
+
+  const humidity = document.getElementById("today-weather-sct-humidity-value");
+  humidity.textContent = weather.humidity;
+
+  const pressure = document.getElementById("today-weather-sct-pressure-value");
+  pressure.textContent = weather.pressure;
+
+  const uvLevel = document.getElementById("today-weather-sct-uv-value");
+  uvLevel.textContent = weather.uvLevele;
+};
+
+export { WeatherDataHandler, WeatherDay, updateData };
