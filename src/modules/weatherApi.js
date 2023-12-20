3515fd96833ae6de84aaa1fa028ce04eaf1f447b
Toma Bogdan <tbogdane@TBE>
2023-12-20 19:48:00 +0200

    fixed bug where cities already added, could be added again

--- src/modules/weatherApi.js
+++ src/modules/weatherApi.js
@@ -10,6 +10,9 @@ const API_KEY = "6c1dab62db374a8ca86125637231412";
 let weatherData = null;
 
 const getWeatherData = async (address, update) => {
+  if (weatherLocationList.includes(address) && update == true) {
+    return;
+  }
   try {
     const responseForecast = await fetch(
       `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${address}&days=7`,
